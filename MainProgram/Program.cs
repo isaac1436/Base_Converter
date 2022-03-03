fb3587560e952f8aaaf7e9c62eec0ed229b32109
ISAAC DESMOND NRUEM <isaac1436@student.babcock.edu.ng>
2022-03-03 15:40:50 +0100

    Final Update with A patch for HexaDecimal

--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -10,12 +10,30 @@
         Console.Write("\n\nPlease enter the base youd like to convert to : ");
         int @base = int.Parse(Console.ReadLine());
 
-        Console.Write("\n\nPlease enter the number to be converted : ");
-        int dec1 = int.Parse(Console.ReadLine());
+        if (@base != 16 && @basePrime != 16)
+        {
+            Console.Write("\n\nPlease enter the number to be converted : ");
+            int num1 = int.Parse(Console.ReadLine());
+            int result = AOB2dec(num1, @basePrime);
 
-        int result = AOB2dec(dec1, @basePrime);
+            dec2AOB(result, @base);
+        }
 
-        dec2AOB(result, @base);
+        else if (@basePrime == 16)
+        {
+            Console.Write("\n\nPlease enter the number to be converted : ");
+            string num1 = Console.ReadLine();
+            int result = Convert.ToInt32(num1,16);
+            dec2AOB(result, @base);
+        }
+
+        else if (@base == 16)
+        {
+            Console.Write("\n\nPlease enter the number to be converted : ");
+            int num1 = int.Parse(Console.ReadLine());
+            int result = AOB2dec(num1, @basePrime);
+            dec2hex(result);
+        }
     }
 
     public static int AOB2dec(int num, int @basePrime)
@@ -64,5 +82,40 @@
             Console.Write(digits[j]);
         }
         return digits;
+    }
+
+    public static int dec2hex(int num)
+    {
+        string[] hexNum = new string[32];
+        int no = num;
+        int[] digits = new int[32];
+        int[] hexNo = new int[32];
+        int i = 0;
+        int count = 0;
+
+        while (no != 0)
+        {
+            Math.DivRem(no, 16, out hexNo[i]);
+            hexNum[i] = Convert.ToString(hexNo[i]);
+
+            if (hexNo[i] == 10) { hexNum[i] = "A"; }
+            else if (hexNo[i] == 11) { hexNum[i] = "B"; }
+            else if (hexNo[i] == 12) { hexNum[i] = "C"; }
+            else if (hexNo[i] == 13) { hexNum[i] = "D"; }
+            else if (hexNo[i] == 14) { hexNum[i] = "E"; }
+            else if (hexNo[i] == 15) { hexNum[i] = "F"; }
+
+            i++;
+            count++;
+            no /= 16;
+        }
+
+        Console.Write("The number entered in base {0} is ", 16);
+        for (int j = count - 1; j >= 0; j--)
+        {
+            Console.Write(hexNum[j]);
+        }
+
+        return no;
     }
 }
