529d44a9e491575e3f22f8f078a4c91013df86c5
ISAAC DESMOND NRUEM <isaac1436@student.babcock.edu.ng>
2022-03-07 09:20:00 +0100

    Exercise 10 and 14

---
+++ Exercise10/Program.cs
@@ -0,0 +1,38 @@
+﻿class Exercise10
+{
+    static void Main()
+    {
+        Console.WriteLine("This is a program to convert from binary to decimal using the Horner Scheme");
+        Console.Write("Please enter the Binary Number to be converted: ");
+        int binaryNum=int.Parse(Console.ReadLine());
+        Console.WriteLine("The number in decimal is " + Bin2dec(binaryNum));
+    }
+
+    public static int Bin2dec(int num)
+    {
+        int decNo = 0;
+        int no = num;
+        int[] digits = new int[32];
+        int i = 0;
+        int count = 0;
+
+        while (no != 0)
+        {
+            Math.DivRem(no, 10, out digits[i]);
+            i++;
+            count++;
+            no /= 10;
+        }
+
+        for (int j = count; j > 0; j--)
+        {
+            decNo += (int)(digits[j]);
+            decNo *= 2;
+        }
+        if (digits[0] == 1)
+        {
+            decNo++;
+        }
+        return decNo;
+    }
+}
---
+++ Exercise14/Program.cs
@@ -0,0 +1,24 @@
+﻿class Exercise14
+{
+    static void Main()
+    {
+        float @float = 0.000001f;
+        float @floatRes = 0.0f;
+
+        double @double = 0.000001d;
+        double @doubleRes = 0.0d;
+
+        decimal @decimal = 0.000001m;
+        decimal @decimalRes = 0.0m;
+
+        for (int i = 0; i < 50000000; i++)
+        {
+            @floatRes += @float;
+            @doubleRes += @double;
+            @decimalRes += @decimal;
+        }
+        Console.WriteLine(@floatRes);
+        Console.WriteLine(@doubleRes);
+        Console.WriteLine(@decimalRes);
+    }
+}
